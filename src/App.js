e8051ecc810bdcbcfb5330b5fdc3e77b187c6696
Abhisar Jain <abhisarjain01@gmail.com>
2026-03-04 00:18:42 +0530

    Added intro page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import IntroPage from './pages/intropage/IntroPage';
+
+function App() {
+  return (
+    <>
+     <IntroPage/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/intropage/IntroPage.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useState } from "react";
+import introVideo from "../../assets/videos/intrro.webm";
+import "./components/IntroPage.css";
+
+export default function IntroPage() {
+
+    const [moveLeft, setMoveLeft] = useState(false);
+    const [displayText, setDisplayText] = useState("");
+    const [showTyping, setShowTyping] = useState(false);
+    const [showBio, setShowBio] = useState(false);
+    const [bubbleText, setBubbleText] = useState("Hello there 👋");
+    const fullText = "ABHISAR JAIN";
+
+    useEffect(() => {
+
+    // move video left
+    setTimeout(() => {
+        setMoveLeft(true);
+    }, 1600);
+
+    // change bubble text
+    setTimeout(() => {
+        setBubbleText("How are you doing?");
+    }, 5000);
+
+    // start typing
+    let i = 0;
+    setTimeout(() => {
+
+        setShowTyping(true);
+
+        const interval = setInterval(() => {
+
+            setDisplayText(fullText.slice(0, i + 1));
+            i++;
+
+            if (i === fullText.length) {
+                clearInterval(interval);
+
+                setTimeout(() => {
+                    setShowBio(true);
+                }, 500);
+            }
+
+        }, 180);
+
+    }, 2600);
+
+}, []);
+    return (
+        <div className="intro-container">
+
+            <div className={`video-wrapper ${moveLeft ? "move-left" : ""}`}>
+                {/* speech bubble */}
+                <div className="speech-bubble">
+                    {bubbleText}
+                </div>
+                <video autoPlay muted playsInline className="intro-video">
+                    <source src={introVideo} type="video/webm" />
+                </video>
+            </div>
+
+            {showTyping && (
+                <div className="intro-card">
+
+                    <h1 className="intro-small">
+                        My Name Is
+                    </h1>
+
+                    <h1 className="intro-heading">
+                        {displayText}
+                        <span className="cursor">|</span>
+                    </h1>
+
+                </div>
+
+            )}
+            {showBio && (
+                <div className="bio-card">
+
+                    <h1 className="bio-text">
+                        I am a Full Stack Developer specializing in Spring Boot, React, and AWS.
+                        I build scalable, secure, and high-performance applications with clean architecture
+                        and modern user experiences.
+                    </h1>
+
+                    <button className="start-btn">
+                        Let's Get Started
+                    </button>
+
+                </div>
+            )}
+
+        </div>
+    );
+}
